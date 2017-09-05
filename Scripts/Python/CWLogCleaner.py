55054e680a3569eb223401eae10d6c9b6d202371
Seth Floyd Jr <sethfloydjr@users.noreply.github.com>
2017-09-05 15:11:51 -0400

    Update CWLogCleaner.py

--- Scripts/Python/CWLogCleaner.py
+++ Scripts/Python/CWLogCleaner.py
@@ -2,7 +2,7 @@ import boto3
 import json
 
 prefix = raw_input('Enter the log name prefix you wish to search through: ')
-#should verify that the user input something, if not then prompt or exit
+#NEED: should verify that the user input something, if not then prompt or exit
 
 #Connect to AWS using default AWS credentials in awscli config
 cwlogs = boto3.client('logs')
@@ -19,10 +19,11 @@ with open('loglist.json', 'w') as outfile:
 with open("loglist.json") as f:
  	file_parsed = json.load(f)
 
+#Prints to the screen what log groups are about to be deleted
 for i in file_parsed['logGroups']:
 	print i['logGroupName']
 
-#Decision point should be added here to prompt user to delete logs or not
+#NEED: Decision point should be added here to prompt user to delete logs or not
 
 #Deletes the log groups that begin with the given prefix
 for i in file_parsed['logGroups']:
